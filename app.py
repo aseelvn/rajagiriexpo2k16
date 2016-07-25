4424e7a04409c221e5363a634b3347c5f7535114
aseelvn <aseelvnazeer@gmail.com>
2016-07-25 18:41:01 +0530

    make it better

--- app.py
+++ app.py
@@ -1,10 +1,63 @@
-from flask import Flask
-from flask import Flask, request, render_template
+from flask import Flask, render_template, request,redirect
+#from flask.ext.mysql import MySQL
+#from werkzeug.wsgi import LimitedStream
 
-app=Flask(__name__)
+#mysql = MySQL()
+app = Flask(__name__)
+
+# MySQL configurations
+
+#app.config['MYSQL_DATABASE_USER'] = 'root'
+#app.config['MYSQL_DATABASE_PASSWORD'] = 'mysqlaccount'
+#app.config['MYSQL_DATABASE_DB'] = 'expo'
+#app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+#mysql.init_app(app)
 
 @app.route('/')
-def index():
-	return render_template("index.html")
-if __name__=="__main__":
-	app.run()
+def main():
+    return render_template('signin.html')
+
+
+@app.route('/showIndex')
+def showIndex():
+    return render_template('index.html')
+
+
+@app.route('/validateLogin',methods=['POST'])
+def validateLogin():
+    try:
+        
+        _password = request.form['inputPassword']
+        if (_password in ("abcd","abcde")):
+            return redirect('/showIndex')
+        else:
+            return render_template("error.html")
+        
+
+       # con = mysql.connect()
+        #cursor = con.cursor()
+        #cursor.callproc('sp_validateLogin',(_password,))
+        #data = cursor.fetchall()
+
+        
+
+
+        #if len(data) > 0:
+         #   if (str(data[0][0])==_password):
+                
+          #      return redirect('/showIndex')
+           # else:
+            #    return render_template('error.html')
+        #else:
+         #   return render_template('error.html')
+            
+
+    except Exception as e:
+        return render_template('error.html',error = str(e))
+    #finally:
+       # cursor.close()
+        #con.close()
+
+
+if __name__ == "__main__":
+    app.run()
